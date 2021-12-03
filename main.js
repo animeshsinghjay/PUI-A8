573df510235789ca8dab8c518af781cfcbe01759
Animesh Singh <animesh.singh.jay@gmail.com>
2021-12-03 15:03:20 -0500

    accessibility changes

---
+++ main.js
@@ -0,0 +1,6 @@
+// $('#fileToUpload').bind('keypress keyup', function(e) {
+//     if(e.which === 32 || e.which === 13){
+//       e.preventDefault();
+//       $('#fileToUpload').click();
+//     }    
+// });
